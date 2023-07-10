e8968e935cdd9be50aa37cf0df73fbfc583255d6
Zainal Salamun <zainalrtf@gmail.com>
2023-07-10 17:14:57 +0700

    splash screen

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:bank_salamu_master/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+import 'onboarding/onboarding_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img_logo_dark.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
