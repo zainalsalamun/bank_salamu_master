920a85f8a37c880033116805ab872669e40e13f5
Zainal Salamun <zainalrtf@gmail.com>
2023-07-11 15:59:43 +0700

    update route main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
-import 'package:bank_salamu_master/ui/pages/splash_page.dart';
+import 'package:bank_salamu_master/ui/pages/home/home_page.dart';
+import 'package:bank_salamu_master/ui/pages/onboarding/onboarding_page.dart';
+import 'package:bank_salamu_master/ui/pages/signin/signin_page.dart';
+import 'package:bank_salamu_master/ui/pages/signup/sign_up_page.dart';
+import 'package:bank_salamu_master/ui/pages/signup/sign_up_set_ktp.dart';
+import 'package:bank_salamu_master/ui/pages/signup/sign_up_set_profile.dart';
+import 'package:bank_salamu_master/ui/pages/signup/sign_up_success_page.dart';
+import 'package:bank_salamu_master/ui/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +17,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/onboarding': (context) => const OnboardingPage(),
+        '/signin': (context) => const SignInPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/signup-set-profile': (context) => const SignUpSetProfile(),
+        '/signup-set-ktp': (context) => const SignUpSetKtp(),
+        '/signup-success': (context) => const SignUpSuccess(),
+        '/home-page': (context) => const HomePage(),
+      },
     );
   }
 }
