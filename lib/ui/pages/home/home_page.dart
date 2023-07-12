73f0cd5d37b4a0d2dec499e4988f17634f5b782f
Zainal Salamun <zainalrtf@gmail.com>
2023-07-12 15:45:56 +0700

    update homepage

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,3 +1,7 @@
+import 'package:bank_salamu_master/ui/widget/home_latest_transaction_item.dart';
+import 'package:bank_salamu_master/ui/widget/home_service_item.dart';
+import 'package:bank_salamu_master/ui/widget/home_tips_item.dart';
+import 'package:bank_salamu_master/ui/widget/home_user_item.dart';
 import 'package:flutter/material.dart';
 
 import '../../../shared/theme.dart';
@@ -79,6 +83,12 @@ class HomePage extends StatelessWidget {
           ),
           children: [
             buildProfile(),
+            buildWalletCard(),
+            buildLevel(),
+            buildServices(),
+            buildLatestTransactions(),
+            buildSendAgain(),
+            buildFriendlyTips(),
           ]),
     );
   }
@@ -138,6 +148,298 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard() {
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/img_bg_card.png',
+          ),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Zainal Salamun',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+          const SizedBox(height: 28),
+          Text(
+            '**** **** **** 1234',
+            style: whiteTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+              letterSpacing: 6,
+            ),
+          ),
+          const SizedBox(height: 21),
+          Text(
+            'Balance',
+            style: whiteTextStyle,
+          ),
+          Text(
+            'Rp 10.000.000',
+            style: whiteTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: semiBold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLevel() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 20,
+      ),
+      padding: const EdgeInsets.all(22),
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Level 1',
+                style: blackTextStyle.copyWith(
+                  fontWeight: medium,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                'Next 55%',
+                style: greenTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+              const SizedBox(width: 4),
+              Text(
+                'of Rp 20.000',
+                style: blackTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(55),
+            child: LinearProgressIndicator(
+              minHeight: 5,
+              value: 0.55,
+              valueColor: AlwaysStoppedAnimation(greenColor),
+              backgroundColor: lightBackgroundColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildServices() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Do Something',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(height: 14),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              HomeServiceItem(
+                  iconUrl: 'assets/ic_topup.png',
+                  title: 'Top Up',
+                  onTap: () {}),
+              HomeServiceItem(
+                  iconUrl: 'assets/ic_send.png', title: 'Send ', onTap: () {}),
+              HomeServiceItem(
+                  iconUrl: 'assets/ic_withdraw.png',
+                  title: 'Withdraw',
+                  onTap: () {}),
+              HomeServiceItem(
+                  iconUrl: 'assets/ic_more.png', title: 'More', onTap: () {}),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildLatestTransactions() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Latest Transactions',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            margin: const EdgeInsets.only(
+              top: 14,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                HomeLatTransactioItem(
+                    iconUrl: 'assets/ic_transaction_cat1.png',
+                    title: 'Top Up',
+                    time: 'Yesterday',
+                    value: '+450.000'),
+                HomeLatTransactioItem(
+                    iconUrl: 'assets/ic_transaction_cat2.png',
+                    title: 'Cashback',
+                    time: 'Sep 11',
+                    value: '+22.000'),
+                HomeLatTransactioItem(
+                    iconUrl: 'assets/ic_transaction_cat3.png',
+                    title: 'Witdraw',
+                    time: 'Aug 27',
+                    value: '-150.000'),
+                HomeLatTransactioItem(
+                    iconUrl: 'assets/ic_transaction_cat4.png',
+                    title: 'transfer',
+                    time: 'Yesterday',
+                    value: '-100.000'),
+                HomeLatTransactioItem(
+                    iconUrl: 'assets/ic_transaction_cat5.png',
+                    title: 'Electric',
+                    time: 'Feb 18',
+                    value: '-180.000')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildSendAgain() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Send Again',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(height: 14),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: const [
+                HomeUserItem(
+                  imageUrl: 'assets/img_friend1.png',
+                  userName: 'Yuanita',
+                ),
+                HomeUserItem(
+                  imageUrl: 'assets/img_friend2.png',
+                  userName: 'Jani',
+                ),
+                HomeUserItem(
+                  imageUrl: 'assets/img_friend3.png',
+                  userName: 'Urip',
+                ),
+                HomeUserItem(
+                  imageUrl: 'assets/img_friend4.png',
+                  userName: 'Masa',
+                ),
+                HomeUserItem(
+                  imageUrl: 'assets/img_friend1.png',
+                  userName: 'Yuanita',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildFriendlyTips() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 30,
+        bottom: 50,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Friendly Tips',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Wrap(spacing: 17, runSpacing: 17, children: const [
+            HomeTipsItem(
+                imageUrl: 'assets/img_tips1.png',
+                title: 'Please image tips',
+                url: 'https//google.com'),
+            HomeTipsItem(
+                imageUrl: 'assets/img_tips2.png',
+                title: 'Please image tips',
+                url: 'https//google.com'),
+            HomeTipsItem(
+                imageUrl: 'assets/img_tips3.png',
+                title: 'Please image tips',
+                url: 'https//google.com'),
+            HomeTipsItem(
+                imageUrl: 'assets/img_tips4.png',
+                title: 'Please image tips',
+                url: 'https//google.com'),
+          ])
         ],
       ),
     );
