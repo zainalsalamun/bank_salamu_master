55bb2ad9870122726dc9331585a9b0f203a72fca
Zainal Salamun <zainalrtf@gmail.com>
2023-07-17 12:34:03 +0700

    change theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bank_salamu_master/shared/theme.dart';
 import 'package:bank_salamu_master/ui/pages/home/home_page.dart';
 import 'package:bank_salamu_master/ui/pages/onboarding/onboarding_page.dart';
 import 'package:bank_salamu_master/ui/pages/signin/signin_page.dart';
@@ -6,7 +7,10 @@ import 'package:bank_salamu_master/ui/pages/signup/sign_up_set_ktp.dart';
 import 'package:bank_salamu_master/ui/pages/signup/sign_up_set_profile.dart';
 import 'package:bank_salamu_master/ui/pages/signup/sign_up_success_page.dart';
 import 'package:bank_salamu_master/ui/pages/splash/splash_page.dart';
+import 'package:bank_salamu_master/ui/pages/topup/pin_page.dart';
 import 'package:flutter/material.dart';
+
+import 'ui/pages/profile/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +23,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: lightBackgroundColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: lightBackgroundColor,
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: blackColor),
+          titleTextStyle:
+              blackTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+        ),
+      ),
       routes: {
         '/': (context) => const SplashPage(),
         '/onboarding': (context) => const OnboardingPage(),
@@ -28,6 +43,8 @@ class MyApp extends StatelessWidget {
         '/signup-set-ktp': (context) => const SignUpSetKtp(),
         '/signup-success': (context) => const SignUpSuccess(),
         '/home-page': (context) => const HomePage(),
+        '/profile': (context) => const ProfilePage(),
+        '/pin-page': (context) => const PinPage(),
       },
     );
   }
